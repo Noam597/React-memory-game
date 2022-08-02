11cfa98d584dc1221880f783836046045e0142fe
Noam <noam_harris@hotmail.com>
2022-08-02 18:33:51 +0300

    new commit

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import styles from './about.module.css'
+import profile from '../../images/profile.jpg'
 const About = () => {
   return (
     <div className={styles.about}>
-       <h1> About</h1>
+       <div className={styles.image}>
+        <div><img src={profile} alt='profile'/></div>
+        
+       </div>
+       <div className={styles.aboutMe}>
+       <h2>created by:</h2>
+       <h1> Noam Harris</h1>
+       <p>
+        Memory Games on multiple difficulties fun for both kids and adults .
+        <br/>
+        play at your own pace or race against time with the least amount of clicks possible!
+        <br/>
+        And remember to have fun!
+       </p>
+       </div>
     </div>
   )
 }
